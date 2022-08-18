826a65aec73b9b4d184f5ed5226b7f49aa75ba78
Anshul Kumar Yadav <rishabhyadav3602@gmail.com>
2022-08-19 02:01:33 +0530

    updaate

--- js/contact.js
+++ js/contact.js
@@ -1,30 +1,28 @@
-let submit = document.getElementById('send_msg').addEventListener("click", function() {
-    // e.preventDefault();
+let submit = document.getElementById('send_msg').addEventListener("submit", function(e) {
+    e.preventDefault();
     
     let visitorName = document.getElementById('name').value;
     let visitoremail = document.getElementById('email').value;
     let visitorsubject = document.getElementById('subject').value;
     let visitormessage = document.getElementById('message').value;
 
-
-    fetch('http://portfoliodata-env.eba-pttt9qzp.us-west-2.elasticbeanstalk.com/visiter', {
-  method: 'POST',
-  body: JSON.stringify({
-    name:visitorName,
-    email:visitoremail,
-    subject: visitorsubject,
-    message: visitormessage
-  }),
-  headers: {
-    'Content-type': 'application/json;',
-  },
-})
-  .then(function(response){
-    return response.json();
-  })
-  .then(function(data){
-    console.log(data);
-  })
+    
+    fetch('https://myporfolio-api.herokuapp.com/visiter', {
+        method: 'POST',
+        body: JSON.stringify({
+            name:visitorName,
+            email:visitoremail,
+            subject: visitorsubject,
+            message: visitormessage
+         
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+        mode:'no-cors'
+      })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
     
 
 });
