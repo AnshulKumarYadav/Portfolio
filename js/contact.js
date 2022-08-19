34e5a63095a1187d91024e5ecb280e4834cb00ba
Anshul Kumar Yadav <rishabhyadav3602@gmail.com>
2022-08-19 15:02:34 +0530

    contact change

--- js/contact.js
+++ js/contact.js
@@ -1,28 +1,29 @@
-let submit = document.getElementById('send_msg').addEventListener("submit", function(e) {
-    e.preventDefault();
-    
-    let visitorName = document.getElementById('name').value;
-    let visitoremail = document.getElementById('email').value;
-    let visitorsubject = document.getElementById('subject').value;
-    let visitormessage = document.getElementById('message').value;
+let register = async (e) => {
+  e.preventDefault();
 
-    
-    fetch('https://myporfolio-api.herokuapp.com/visiter', {
-        method: 'POST',
-        body: JSON.stringify({
-            name:visitorName,
-            email:visitoremail,
-            subject: visitorsubject,
-            message: visitormessage
-         
-        }),
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-        },
-        mode:'no-cors'
-      })
-        .then((response) => response.json())
-        .then((json) => console.log(json));
-    
+  let form_data = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("message").value,
+  };
 
-});
+  form_data = JSON.stringify(form_data);
+
+  let res = await fetch(
+    "https://myporfolio-api.herokuapp.com/visiter",
+    {
+      method: "POST",
+      body: form_data,
+      mode: "no-cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  let data = await res.json();
+  console.log(data);
+};
+
+document.getElementById("send_msg").addEventListener("click", register);
